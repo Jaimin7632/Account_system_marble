891ca5cbbca2a4121b094680d7bd771a2384e9e2
jaimin <jaiminpatel9192@gmail.com>
2020-06-16 11:38:29 +0530

    add multicell in generate pdf

--- generate_pdf/generate_pdf.php
+++ generate_pdf/generate_pdf.php
@@ -1,48 +1,68 @@
 <?php
-include('fpdf182/fpdf182.php');
+include('mc_table.php');
 
 $data = $_POST['data'];
 $y = json_decode($data,true);
 
 
 
-$pdf = new FPDF();
+$pdf=new PDF_MC_Table();
 $pdf->AddPage();
+
 $pdf->SetFont('Arial','B',8);
+$cell_width = 24;
 
 #Print_date
 $date = "Date: ".date("d/m/Y");
-$pdf->Cell(20,10,$date);
+$pdf->Cell($cell_width,10,$date);
 $pdf->ln();
 
 
-$ignore_list = array('comment','bill_id','details','is_bill','pay_receive');
+$ignore_list = array('comment','bill_id','details','is_bill','pay_receive','id');
+
+
+
 $keys = array_keys($y[0]);
+
+$pdf->Cell($cell_width,10,'#','LRTB','C');
 foreach($keys as $key){
 	if(in_array(strtolower($key), $ignore_list)){
 		continue;
 	}
-	$pdf->Cell(24,10,$key,'LRTB','C');
+	$pdf->Cell($cell_width,10,$key,'LRTB','C');
 	// print_r($key);
 }
 
 $pdf->ln();
 $pdf->SetFont('Arial','',8);
 
+
+$id = 1;
 foreach ($y as $key => $value) {
+	$row = array();
+	$width=array();
+	$row[] = $id++;
+	$width[] = $cell_width;
 	foreach ($value as $key2 => $value2) {
 		if(in_array(strtolower($key2), $ignore_list)){
 			continue;
 		}
-		$pdf->Cell(24,7,$value2,'LRTB','C');
+
+		$row[] = $value2;
+		$width[] = $cell_width;
+		// $pdf->Cell(24,7,$value2,'LRTB','C');
 	}
-	$pdf->ln();
+	$pdf->SetWidths($width);
+	$pdf->Row($row);
+	// $pdf->ln();
 }
+
+$pdf->ln();
 
 	$pdf->ln();
 	$pdf->ln();
 	if(isset($_POST['footer_data'])){
-		$pdf->Cell(24,7,$_POST['footer_data']);
+		$pdf->Cell($cell_width,7,$_POST['footer_data']);
 	}
 
 
